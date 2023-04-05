77fcd542398f812e710f4b9776ec6d96301b8669
Qingying Meng <qmeng222@gmail.com>
2023-04-05 00:11:16 -0700

    rendered css, bike image, and go back icon

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ const server = http.createServer(async (req, res) => {
   const id = myUrl.searchParams.get("id");
   // console.log(pathname, id); // /bicycle 1
 
+  // console.log(req.url);
+  // // /bicycle?id=0
+  // // /index.css
+  // // /0.png
+  // // /icons.svg
+
   if (pathname === "/") {
     const html = await fs.readFile("./view/bicycles.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
@@ -38,6 +44,18 @@ const server = http.createServer(async (req, res) => {
     const html = await fs.readFile("./view/overview.html", "utf-8");
     res.writeHead(200, { "Content-Type": "text/html" });
     res.end(html);
+  } else if (/\.(png)$/i.test(req.url)) {
+    const image = await fs.readFile(`./public/images/${req.url.slice(1)}`);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    res.end(image);
+  } else if (/\.(css)$/i.test(req.url)) {
+    const css = await fs.readFile("./public/css/index.css");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(css);
+  } else if (/\.(svg)$/i.test(req.url)) {
+    const svg = await fs.readFile(`./public/images/icons.svg`);
+    res.writeHead(200, { "Content-Type": "image/svg+xml" });
+    res.end(svg);
   } else {
     res.writeHead(404, { "Content-Type": "text/html" });
     res.end(`<div><h1>File Not Found</h1></div>`);
